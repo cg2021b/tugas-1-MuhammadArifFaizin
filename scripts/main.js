ad7af2208b9ca48ab89d2e912c8da150853469fb
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2021-10-05 19:54:48 +0700

    create Gallon with Ellipse

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,24 @@ import Color from "./webgl/Color.js";
 
 let scene;
 
-function main() {
-  const canvas = document.getElementById("myCanvas");
-  scene = new Scene(canvas);
+const createGallon = (x, y) => {
+  const topGallons = [];
+  for (let i = 0; i < 20; i++) {
+    topGallons.push({
+      centerX: x,
+      centerY: y + 0.3 - (i * (0.3 - 0.1)) / 20,
+      centerZ: 0,
+      radiusX: 6 + Math.pow(i * 0.15, 2),
+      radiusY: 4 + (i * (12 - 4)) / 20,
+      numberOfSides: 120,
+      uniformColors: new Color(0.5, 0.5, 0.8 + (i * (0.84 - 0.8)) / 20, 1.0),
+    });
+  }
 
-  const ellipses = [
+  const bottomGallons = [
     {
-      centerX: 0,
-      centerY: -0.3,
+      centerX: x,
+      centerY: y + -0.3,
       centerZ: 0,
       radiusX: 22,
       radiusY: 12,
@@ -20,8 +30,8 @@ function main() {
       uniformColors: new Color(0.5, 0.5, 1.0, 1.0),
     },
     {
-      centerX: 0,
-      centerY: -0.2,
+      centerX: x,
+      centerY: y + -0.2,
       centerZ: 0,
       radiusX: 22,
       radiusY: 12,
@@ -29,8 +39,8 @@ function main() {
       uniformColors: new Color(0.5, 0.5, 0.96, 1.0),
     },
     {
-      centerX: 0,
-      centerY: -0.1,
+      centerX: x,
+      centerY: y + -0.1,
       centerZ: 0,
       radiusX: 22,
       radiusY: 12,
@@ -38,8 +48,8 @@ function main() {
       uniformColors: new Color(0.5, 0.5, 0.92, 1.0),
     },
     {
-      centerX: 0,
-      centerY: 0,
+      centerX: x,
+      centerY: y + 0,
       centerZ: 0,
       radiusX: 22,
       radiusY: 12,
@@ -47,26 +57,32 @@ function main() {
       uniformColors: new Color(0.5, 0.5, 0.88, 1.0),
     },
     {
-      centerX: 0,
-      centerY: 0.1,
+      centerX: x,
+      centerY: y + 0.1,
       centerZ: 0,
       radiusX: 22,
       radiusY: 12,
       numberOfSides: 120,
       uniformColors: new Color(0.5, 0.5, 0.84, 1.0),
     },
-    {
-      centerX: 0,
-      centerY: 0.3,
-      centerZ: 0,
-      radiusX: 6,
-      radiusY: 4,
-      numberOfSides: 120,
-      uniformColors: new Color(0.5, 0.5, 0.8, 1.0),
-    },
   ];
 
-  ellipses.forEach((item) => {
+  return [...bottomGallons, ...topGallons];
+};
+
+function main() {
+  const canvas = document.getElementById("myCanvas");
+  scene = new Scene(canvas);
+
+  const bgColor = new Color(249 / 255, 249 / 255, 237 / 255, 1.0);
+  scene.setBgColor(bgColor);
+
+  const gallon1 = createGallon(-0.5, 0);
+  const gallon2 = createGallon(0.5, 0);
+
+  const gallons = [...gallon1, ...gallon2];
+
+  gallons.forEach((item) => {
     const ellipse = new Ellipse(
       new Vector3(item.centerX, item.centerY, item.centerZ),
       item.radiusX,
@@ -80,7 +96,7 @@ function main() {
 
   scene.render();
 
-  //   animate();
+  // animate();
 }
 
 function update() {
