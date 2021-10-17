1d05b3dae1c09f34773665959021834b91857798
Muhammad Arif Faizin <muhammadariffaizin@gmail.com>
2021-10-17 14:52:00 +0700

    Add auto generate cube with check objects for two same objects

--- User Interaction Three Js/scripts/main.js
+++ User Interaction Three Js/scripts/main.js
@@ -1,7 +1,8 @@
 import { getRandomInt } from "./utils.js";
 
 let scene, canvas, camera, renderer, controls;
-let groups = [],
+let selectedObjects = [],
+  originalColors = [],
   geometries = [];
 
 const BG_COLOR = 0x151515;
@@ -20,6 +21,51 @@ const KEYCODE = {
 const colorChoices = [
   0xc06ec7, 0xb8171c, 0x6fa64a, 0xbaba66, 0x7f7dcb, 0xad2749,
 ];
+
+// Create Geometry Function
+const createCube = (side, x, y, z, color) => {
+  const geometry = new THREE.BoxGeometry(side, side, side);
+  const material = new THREE.MeshPhysicalMaterial({ color: color });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.set(x, y, z);
+
+  return cube;
+};
+
+// Generate Random Cube
+const generateCube = () => {
+  const obj = createCube(
+    getRandomInt(2, 5),
+    getRandomInt(-10, 10),
+    getRandomInt(-10, 10),
+    getRandomInt(0, 10),
+    colorChoices[getRandomInt(0, colorChoices.length)]
+  );
+
+  geometries.push(obj);
+  scene.add(obj);
+};
+
+const checkObject = () => {
+  const firstObjColor = originalColors[0];
+  const secondObjColor = originalColors[1];
+
+  console.log(firstObjColor, secondObjColor);
+  if (firstObjColor === secondObjColor) {
+    selectedObjects.forEach((obj) => {
+      obj.geometry.dispose();
+      obj.material.dispose();
+      scene.remove(obj);
+      renderer.renderLists.dispose();
+    });
+  } else {
+    selectedObjects[0].material.emissive.setHex(0x000000);
+    selectedObjects[1].material.emissive.setHex(0x000000);
+  }
+
+  selectedObjects = [];
+  originalColors = [];
+};
 
 const main = () => {
   let raycaster = new THREE.Raycaster();
@@ -66,18 +112,23 @@ const main = () => {
     // calculate objects intersecting the picking ray
     let intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects && intersects[0]) {
-      intersects.forEach((obj) => obj.object.material.color.set(0x00ff00));
+      const firstObj = intersects[0].object;
+      if (selectedObjects.length > 0) {
+        if (firstObj.uuid === selectedObjects[0].uuid) {
+          firstObj.material.emissive.setHex(0x000000);
+          selectedObjects = [];
+          originalColors = [];
+          return;
+        }
+      }
+
+      selectedObjects.push(firstObj);
+      originalColors.push(firstObj.material.color.getHex());
+      console.log(selectedObjects);
+      if (selectedObjects.length > 1) {
+        checkObject();
+      }
     }
-  };
-
-  // Create Geometry Function
-  const createCube = (side, x, y, z, color) => {
-    const geometry = new THREE.BoxGeometry(side, side, side);
-    const material = new THREE.MeshPhysicalMaterial({ color: color });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(x, y, z);
-
-    return cube;
   };
 
   // 1. Create the scene
@@ -100,21 +151,7 @@ const main = () => {
   // 3. Create an locate the object on the scene
   // Loop with geometryData
   for (let i = 0; i < 10; i++) {
-    const obj = createCube(
-      getRandomInt(2, 5),
-      getRandomInt(-10, 10),
-      getRandomInt(-10, 10),
-      getRandomInt(0, 10),
-      colorChoices[getRandomInt(0, colorChoices.length)]
-    );
-    const group = new THREE.Object3D();
-
-    obj.userData.parent = group;
-    group.add(obj);
-
-    geometries.push(obj);
-    groups.push(group);
-    scene.add(group);
+    generateCube();
   }
 
   camera.position.set(15, 10, 20);
@@ -138,6 +175,12 @@ const main = () => {
   window.addEventListener("resize", onWindowResize);
 };
 
+const clock = new THREE.Clock();
+
+let treshold = 0;
+let speed = 0.002;
+const baseSpeed = 0.002;
+
 const mainLoop = () => {
   renderer.render(scene, camera);
 
@@ -145,6 +188,28 @@ const mainLoop = () => {
     geometry.rotation.x += 0.01;
     geometry.rotation.y += 0.01;
   });
+
+  if (scene.children.length >= 50) {
+    treshold = 0;
+    speed = baseSpeed;
+  } else {
+    treshold += speed;
+  }
+
+  // console.log(`speed is ${speed}, treshold is ${treshold}`);
+  if (treshold > 1) {
+    generateCube();
+    treshold = 0;
+    speed += 0.002;
+    console.log(`new cube, now speed is ${speed}`);
+  }
+
+  const elapsedTime = clock.getElapsedTime();
+  if (selectedObjects.length > 0) {
+    selectedObjects[0].material.emissive.setHex(
+      elapsedTime % 0.5 >= 0.25 ? originalColors[0] : 0x00ff00
+    );
+  }
 
   controls.update();
   requestAnimationFrame(mainLoop);
